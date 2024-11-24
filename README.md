553fa0bfff7abf90588d4e440e0cf434a867a687
Bill Locke <wpl3a@virginia.edu>
2024-11-24 14:43:45 -0500

    2nd install of finalproject package

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # final-project-2024
 DS5100 Final Project Repo
+
+Author: Bill Locke
+Project: DS5100 Final Project
+
+The module is a simple Monte Carlo simulator using a set of 3 related
+classes:
+    1. Die class 
+    2. Game class
+    3. Analyzer class
+    
+The classes are related in the following way: Game objects are initialized with a Die
+object, and Analyzer objects are initialized with a Game object.
+
+    [Die] → [Game] → [Analyzer]
+
+In this simulator, a “die” can be any discrete random variable associated with a
+stochastic process, such as using a deck of cards, flipping a coin, rolling an actual die,
+or speaking a language.
---
+++ finalproject/die.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+
+class Die():
+    '''General Definition
+    A die has 𝑁 sides, or “faces”, and 𝑊 weights,
+    and can be rolled to select a face.
+    For example, a “die” with 𝑁=2 is a coin, and
+    a one with 𝑁=6 is a standard die.
+    Normally, dice and coins are “fair,” meaning that the
+    each side has an equal weight.
+    An unfair die is one where the weights are unequal.
+    Each side contains a unique symbol.
+    Symbols may be all alphabetic or all numeric.
+    𝑊 defaults to 1.0 for each face
+    but can be changed after the object is created.
+    The weights are just positive numbers (integers or floats, including 0),
+    not a normalized probability distribution.
+    The die has one behavior, which is to be rolled one or more times.
+    Specific Methods and Attributes:
+        1. An initializer
+        2. Change the weight of a single side.
+        3. Create die
+        4. Roll die
+        5. Show die's current state
+    '''
+    def __init__(self, faces):
+        self.faces = faces
+        ''' Takes a NumPy array of faces as an argument.'''
+
+        # verify faces is type (np.ndarray); TypeError if not
+        if not isinstance (self.faces, np.ndarray):
+            raise TypeError('faces is not an np array')
+
+        # Tests to see if the values are distinct; ValueError if not
+        if len(self.faces) != len(set(self.faces)):    # 'set' values are unique
+            raise ValueError('faces are not unique')
+            # NOTE: faces = np.unique (faces) removes "redundant" values        
+
+        # Internally initializes the weights to 1.0 for each face.
+        self.weights = np.ones(len(self.faces))
+        #print (faces, weights)
+
+        # Saves both faces and weights in a private data frame
+        #   with faces in the index.
+        index_values = [self.faces]
+        self.faces_df = pd.DataFrame({'weights': self.weights}, index=index_values)
+
+    def change_weight (self, face_to_change, new_weight):
+        '''Takes two arguments: the face value to be changed and the new weight.'''
+        self.face_to_change = face_to_change
+        self.new_weight = new_weight
+
+        # Checks to see if the face passed is valid value,
+        #   i.e. if it is in the die array; IndexError if not
+        if self.face_to_change not in self.faces_df.index:
+#        if self.face_to_change not in self.faces_df.values:
+            raise IndexError('face_to_change not in faces.df')
+
+        # Checks to see if the weight is a valid type,
+        #   i.e. if it is numeric (integer or float); TypeError if not
+        if not isinstance(self.new_weight, (int, float)):
+            raise TypeError('new_weight is not valid type')
+           
+        # change face's weight
+        # subtract 1 to change correct face due to Python offset
+        self.faces_df.loc[face_to_change-1] = self.new_weight
+        
+    def create_die (self, faces_df):
+        '''Create the die using the object's weights. Save to self as a DataFrame.'''
+        n_sides = len(self.faces_df.weights)
+        my_probs = [i/sum(self.faces_df.weights) for i in self.faces_df.weights]
+        self.die = pd.DataFrame({
+        'side': range(1, n_sides + 1),
+        'weights': my_probs
+        })
+        return self.die
+
+    def roll_die (self, number_of_rolls = 1):
+        '''takes a parameter of how many times the die is to be rolled; defaults to 1.'''
+        self.number_of_rolls = number_of_rolls
+        # print (self.die, '\nsum of weights:', sum(self.die.weights))
+        # print ('number of rolls:', self.number_of_rolls)   
+
+        # This is essentially a random sample with replacement,
+        #   from the private die data frame, that applies the weights.
+        results = []
+        for i in range(self.number_of_rolls):
+            result = self.die.side.sample(weights=self.die.weights, replace=True).values[0]
+            results.append(result)
+        self.result = pd.DataFrame(results)
+        print ('results in roll_die:', self.result)
+        return (self.result)
+        #return pd.Series(results)
+        
+    def show_die_state(self, die):
+        '''A method to show the die’s current state.
+        Returns a copy of the private die data frame.'''
+        self.die = die
+        die_deep = self.die.copy()
+        return die_deep
+        
+    def plot_results(self, my_results):
+        '''Show the results of rolling the dice n times with a simple bar graph.'''
+        my_results.value_counts().sort_index().plot.bar(rot=0);
+        # A method to show the die’s current state.
+        #  Returns a copy of the private die data frame.
