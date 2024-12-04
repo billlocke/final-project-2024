56c301ff8e02c56931f2669feae90fa5d569c4e8
Bill Locke <wpl3a@virginia.edu>
2024-12-04 16:06:18 -0500

    12/4 16:06 install of finalproject package

--- finalproject/analyzer.py
+++ finalproject/analyzer.py
@@ -30,7 +30,41 @@ class Analyzer():
             e.g. all ones for a six-sided die.
         Computes how many times the game resulted in a jackpot.
         Returns an integer for the number of jackpots. 
+        
+        ### jackpot PSEUDO CODE:
+        
+        Input is DataFrame with roll # index, 
+            and column for each Die result (face):
+        
+                Die1 Die2 Die3
+        roll #
+            1     6    4    3
+            2     1    3    3
+            3     4    5    1
+            4     1    1    1
+            5     6    2    5
+            
+        jackpot_number = 0
+        For each roll n
+            if Die1 face == Die2 face == Die3 face
+                jackpot_number += 1
+        return jackpot_number
+
+        loop_result_list = []    # contains each die's roll result
+        for i in range(len(self.die_list)):
+            loop_result = self.die_list[i].roll_die(number_of_rolls)
+            loop_result_list.append (loop_result)
+            
+        print ('length of loop_result_list:', len(loop_result_list))
+        print ('loop_result_list:', loop_result_list)
+            
+        self.results1 = self.die_list.roll_die(number_of_rolls)
+        print ('rolled die:') 
+        print ('results:', self.results1)
+
+        ### END jackpot PSEUDO CODE
         '''
+        
         self.number_of_rolls = number_of_rolls
         
         self.results1 = self.die_list.roll_die(self.number_of_rolls)
@@ -49,6 +83,36 @@ class Analyzer():
             face values as columns, 
             and count values in the cells (i.e. it is in wide format).   
            
+        ### face_counts_per_roll PSEUDO CODE:
+        
+        Input is DataFrame with roll # index, 
+            and column for each Die result (face):
+        
+                Die1 Die2 Die3
+        roll #
+            1     6    4    3
+            2     1    3    3
+            3     4    5    1
+            4     1    1    1
+            5     6    2    5
+
+        For each roll n
+            all die_face_count in row <- 0
+            for each Die_n_value
+                face_count[Die_n_value] += 1
+        return results_data_frame (below)
+
+        Results DataFrame
+                Face1 Face2 Face3 Face4 Face5 Face6
+        roll #
+            1     0     0     1     1     0     1
+            2     1     0     2     0     0     0
+            3     1     0     0     1     1     0
+            4     3     0     0     0     0     0
+            5     0     1     0     0     1     1
+                
+        ### END face_counts_per_roll PSEUDO CODE
+
         '''
         
     def combo_count (self, ):
@@ -60,6 +124,66 @@ class Analyzer():
         The data frame should have a MultiIndex of distinct combinations
             and a column for the associated counts.
         
+        ### combo_count PSEUDO CODE:
+        
+        Input is DataFrame with roll # index, 
+            and column for each Die result (face):
+        
+                Die1 Die2 Die3
+        roll #
+            1     6    4    3
+            2     1    3    3
+            3     4    5    1
+            4     1    1    1
+            5     6    2    5
+            6     1    5    4
+
+        face_values_list = []
+        For each roll n
+            populate roll_list with roll's face values
+            sort (face_values)
+            append roll_list to face_values_list
+        sort (face_value_list)
+
+        face_values_list DataFrame:
+
+        roll_list before sorting
+        roll #
+            1  [3, 4, 6]
+            2  [1, 3, 3]
+            3  [1, 4, 5]
+            4  [1, 1, 1]
+            5  [2, 5, 6]
+            6  [1, 4, 5]
+            
+        roll_list after sorting
+        roll #
+            4  [1, 1, 1]
+            2  [1, 3, 3]
+            3  [1, 4, 5]
+            6  [1, 4, 5]
+            5  [2, 5, 6]
+            1  [3, 4, 6]
+        
+        face_value_list = ['roll_list', 'count']
+        for i in range(len(row_list))
+            if row_list[i] not in face_value_list
+                add row_list[i] to face_values_list[0]
+                count[i] <- 1
+            else count[i] += 1
+        return face_values_list
+        
+        combo_list dataframe:
+        
+        roll_list  count
+        [1, 1, 1]    1
+        [1, 3, 3]    1
+        [1, 4, 5]    2
+        [2, 5, 6]    1
+        [3, 4, 6]    1
+                 
+
+        ### END combo_count PSEUDO CODE
         '''
 
         
@@ -71,14 +195,92 @@ class Analyzer():
         Returns a data frame of results.
         The data frame should have a MultiIndex of distinct permutations
         and a column for the associated counts.
-        
-        '''
 
-        '''my_results.value_counts().sort_index().plot.bar(rot=0);
-        NARROW = pd.DataFrame(my_results).stack
-        print ('NARROW:\n', NARROW)
-        WIDE = pd.DataFrame(my_results).unstack()
-        print ('my_results dimensions:', my_results.shape)
-        print ('WIDE dimensions:', WIDE.shape)
-        print ('WIDE:\n', WIDE)
-'''
+        ### permutation_count PSEUDO CODE:
+        
+        Input is DataFrame with roll # index, 
+            and column for each Die result (face):
+        
+                Die1 Die2 Die3
+        roll #
+            1     6    4    3
+            2     1    3    3
+            3     4    5    1
+            4     1    1    1
+            5     6    2    5
+            6     1    5    4
+            7     1    3    3
+
+        face_values_list = []
+        For each roll n
+            populate roll_list with roll's face values
+            sort (face_values)
+            append roll_list to face_values_list
+        sort (face_value_list)
+
+        face_values_list DataFrame:
+
+        roll_list before sorting
+        roll #
+            1  [6, 4, 3]
+            2  [1, 3, 3]
+            3  [4, 5, 1]
+            4  [1, 1, 1]
+            5  [6, 2, 5]
+            6  [1, 5, 4]
+            7  [1, 3, 3]
+            
+        roll_list after sorting
+        roll #
+            4  [1, 1, 1]
+            2  [1, 3, 3]
+            7  [1, 3, 3]
+            6  [1, 5, 4]
+            3  [4, 5, 1]
+            5  [6, 2, 5]
+            1  [6, 4, 3]
+        
+        face_value_list = ['roll_list', 'count']
+        for i in range(len(row_list))
+            if row_list[i] not in face_value_list
+                add row_list[i] to face_values_list[0]
+                count[i] <- 1
+            else count[i] += 1
+        return face_values_list
+        
+        permutation_list dataframe:
+        
+        roll_list  count
+        [1, 1, 1]    1
+        [1, 3, 3]    2
+        [1, 5, 4]    1
+        [4, 5, 1]    1
+        [6, 2, 5]    1
+        [6, 4, 3]    1
+                 
+
+        ### END permutation_count PSEUDO CODE
+        '''
+                
+        self.form = form.lower()
+        if self.form != 'w' or self.form != 'n':
+            raise ValueError('form is not "w" or "n".')
+        return
+
+        # verify faces is type (np.ndarray); TypeError if not
+        if not isinstance (self.faces, np.ndarray):
+            raise TypeError('faces is not an np array')
+
+        # Tests to see if the values are distinct; ValueError if not
+        if len(self.faces) != len(set(self.faces)):    # 'set' values are unique
+            raise ValueError('faces are not unique')
+            # NOTE: faces = np.unique (faces) removes "redundant" values        
+
+        # Internally initializes the weights to 1.0 for each face.
+        self.weights = np.ones(len(self.faces))
+        #print (faces, weights)
+
+        # Saves both faces and weights in a private data frame
+        #   with faces in the index.
+        index_values = [self.faces]
+        self.faces_df = pd.DataFrame({'weights': self.weights}, index=index_values)
